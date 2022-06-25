48bbd6ffa9b53e6ea044a3a3a81a57a1e7b88144
code-codes <92877499+code-codes@users.noreply.github.com>
2022-06-25 22:27:22 +0530

    Update Status.js

--- src/components/Status.js
+++ src/components/Status.js
@@ -1,12 +1,89 @@
 import React from "react";
+import { useState, useEffect } from 'react';
+import axios from "axios";
 
-function Status() {
+function Applications() {
+
+    let [users, setUsers] = useState([{
+        "id": "12330",
+        "StartupName": "Dunzo",
+        "description": "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eligendi non quis exercitationem culpa nesciunt nihil aut nostrum explicabo reprehenderit optio amet ab temporibus asperiores quasi cupiditate. Voluptatum ducimus voluptates voluptas?",
+        "problem": "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eligendi non quis exercitationem culpa nesciunt nihil aut nostrum explicabo reprehenderit optio amet ab temporibus asperiores quasi cupiditate. Voluptatum ducimus voluptates voluptas?",
+        "Solution": "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eligendi non quis exercitationem culpa nesciunt nihil aut nostrum explicabo reprehenderit optio amet ab temporibus asperiores quasi cupiditate. Voluptatum ducimus voluptates voluptas?",
+        "status": "Rejected"
+    },
+    {
+        "id": "12331",
+        "StartupName": "Horizon",
+        "description": "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eligendi non quis exercitationem culpa nesciunt nihil aut nostrum explicabo reprehenderit optio amet ab temporibus asperiores quasi cupiditate. Voluptatum ducimus voluptates voluptas?",
+        "problem": "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eligendi non quis exercitationem culpa nesciunt nihil aut nostrum explicabo reprehenderit optio amet ab temporibus asperiores quasi cupiditate. Voluptatum ducimus voluptates voluptas?",
+        "Solution": "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eligendi non quis exercitationem culpa nesciunt nihil aut nostrum explicabo reprehenderit optio amet ab temporibus asperiores quasi cupiditate. Voluptatum ducimus voluptates voluptas?",
+        "status": "Accepted"
+    },
+    {
+        "id": "12332",
+        "StartupName": "Hustlers",
+        "description": "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eligendi non quis exercitationem culpa nesciunt nihil aut nostrum explicabo reprehenderit optio amet ab temporibus asperiores quasi cupiditate. Voluptatum ducimus voluptates voluptas?",
+        "problem": "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eligendi non quis exercitationem culpa nesciunt nihil aut nostrum explicabo reprehenderit optio amet ab temporibus asperiores quasi cupiditate. Voluptatum ducimus voluptates voluptas?",
+        "Solution": "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eligendi non quis exercitationem culpa nesciunt nihil aut nostrum explicabo reprehenderit optio amet ab temporibus asperiores quasi cupiditate. Voluptatum ducimus voluptates voluptas?",
+        "status": "Rejected"
+    },
+    {
+        "id": "12333",
+        "StartupName": "earthly",
+        "description": "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eligendi non quis exercitationem culpa nesciunt nihil aut nostrum explicabo reprehenderit optio amet ab temporibus asperiores quasi cupiditate. Voluptatum ducimus voluptates voluptas?",
+        "problem": "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eligendi non quis exercitationem culpa nesciunt nihil aut nostrum explicabo reprehenderit optio amet ab temporibus asperiores quasi cupiditate. Voluptatum ducimus voluptates voluptas?",
+        "Solution": "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eligendi non quis exercitationem culpa nesciunt nihil aut nostrum explicabo reprehenderit optio amet ab temporibus asperiores quasi cupiditate. Voluptatum ducimus voluptates voluptas?",
+        "status": "Rejected"
+    }])
 
     return (
-        <div className="text-center">
-            <h1>Status</h1>
+        <div className="container text-center mt-4">
+            <table className="table">
+                <thead>
+                    <tr>
+                        <th>Startup Name</th>
+                        <th>Description</th>
+                        <th>Problem</th>
+                        <th>Solution</th>
+                        <th>Status</th>
+                    </tr>
+                </thead>
+
+                <tbody>
+                    {
+                        users.map((userObj) => <tr key={userObj.id}>
+
+                            <td>
+                                {userObj.StartupName}
+
+                            </td>
+
+                            <td>
+                                {userObj.description}
+
+                            </td>
+
+                            <td>
+                                {userObj.problem}
+
+                            </td>
+                            <td>
+                                {userObj.Solution}
+
+                            </td>
+
+
+                            <td>
+                                    <button className="btn btn-dark m-4" >{userObj.status}</button>
+                                </td>
+                            
+                        </tr>)
+                    }
+                </tbody>
+            </table>
         </div>
-    )
+    );
 }
 
-export default Status;
+export default Applications;
